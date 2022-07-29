7b0f0c3cf8febefeda78fe36df61fafbccb03037
Cassandra Lee <cassichan@Cassandras-MacBook-Air.local>
2022-07-29 14:47:38 -0400

    first group project using express to connect to firestore and POST and GET

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import { getFirestore } from "firebase-admin/firestore";
+import { credentials } from "./credentials.js";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = 4000;
+
+if (getApps().length === 0) {
+  initializeApp({
+    credential: cert(credentials),
+  });
+}
+
+//Connect to FireStore database
+const db = getFirestore();
+
+app.listen(PORT, () => {
+  console.log(`Now listening on http://localhost:${PORT}`);
+});
+
+//Routes
+
+//Test
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+//Add a new celebrity to firestore database
+app.post("/add-celebrity", (req, res) => {
+  const celebrity = req.body;
+  db.collection("celebrities")
+    .add(celebrity)
+    .then((doc) => res.status(201).send({ id: doc.id }))
+    .catch((err) => res.status(500).json(err));
+});
